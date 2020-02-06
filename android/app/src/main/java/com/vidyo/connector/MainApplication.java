115cb64d0f7cc145b9b22099de5ccfd98b1dc137
tmelko-vidyo <tmelko@vidyo.com>
2020-02-06 16:55:44 +0200

    + VidyoClient implementation for Android

--- android/app/src/main/java/com/vidyo/connector/MainApplication.java
+++ android/app/src/main/java/com/vidyo/connector/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.vidyo.connector.view.VidyoConnectorPackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -22,10 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-            @SuppressWarnings("UnnecessaryLocalVariable")
             List<ReactPackage> packages = new PackageList(this).getPackages();
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
+            packages.add(new VidyoConnectorPackage());
             return packages;
         }
 
@@ -50,15 +49,13 @@ public class MainApplication extends Application implements ReactApplication {
     /**
      * Loads Flipper in React Native templates.
      *
-     * @param context
+     * @param context {@link Context}
      */
     private static void initializeFlipper(Context context) {
         if (BuildConfig.DEBUG) {
             try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
+                /* We use reflection here to pick up the class that initializes Flipper,
+                since Flipper library is not available in release mode */
                 Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
                 aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
             } catch (ClassNotFoundException e) {
